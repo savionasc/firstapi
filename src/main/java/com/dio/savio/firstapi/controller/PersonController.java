43340bac9fb08c773e187b173c0e482086597a57
Savio Nascimento <savio.nascimento@beegol.com>
2022-01-06 18:18:23 -0300

    Alterando responsabilidades

--- src/main/java/com/dio/savio/firstapi/controller/PersonController.java
+++ src/main/java/com/dio/savio/firstapi/controller/PersonController.java
@@ -3,25 +3,24 @@ package com.dio.savio.firstapi.controller;
 import com.dio.savio.firstapi.dto.response.MessageResponseDTO;
 import com.dio.savio.firstapi.entity.Person;
 import com.dio.savio.firstapi.repository.PersonRepository;
+import com.dio.savio.firstapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService){
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 }
